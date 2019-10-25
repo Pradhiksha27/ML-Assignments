64d87078e24a942d5f2a221c019a11d16b4e550c
Pradhiksha Ashok Kumar <pashokkumar@umass.edu>
2019-10-25 12:57:55 -0400

    Update Grid_Search.py

--- HW1/Grid_Search.py
+++ HW1/Grid_Search.py
@@ -1,25 +1,25 @@
-def gs_f():
-	import numpy as np
-	import pandas as pd
-	import matplotlib.pyplot as plt
-	from sklearn.metrics import f1_score
-	from sklearn.tree import DecisionTreeClassifier
-	from sklearn.model_selection import cross_val_score
-	from sklearn.model_selection import GridSearchCV
 
-	X_train = pd.read_csv("/home/pradhiksha/Desktop/589/HW1/hw1/Data/x_train.csv", header=None) 
-	y_train = pd.read_csv("/home/pradhiksha/Desktop/589/HW1/hw1/Data/y_train.csv", header=None) 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+
+X_train = pd.read_csv("/home/pradhiksha/Desktop/589/HW1/hw1/Data/x_train.csv", header=None) 
+y_train = pd.read_csv("/home/pradhiksha/Desktop/589/HW1/hw1/Data/y_train.csv", header=None) 
 
 
-	X_train_folds = []
-	y_train_folds = []
+X_train_folds = []
+y_train_folds = []
 
-	clf = DecisionTreeClassifier()
-	param = {'criterion':['gini','entropy'], 'max_depth': [6,7,8,9], 'class_weight' : ['balanced'], 'min_samples_split':[30,40,50,60]}
+clf = DecisionTreeClassifier()
+param = {'criterion':['gini','entropy'], 'max_depth': [6,7,8,9], 'class_weight' : ['balanced'], 'min_samples_split':[30,40,50,60]}
 
-	grid = GridSearchCV(clf, param, cv=5)
-	grid.fit(X_train, y_train)
+grid = GridSearchCV(clf, param, cv=5)
+grid.fit(X_train, y_train)
 
-	best_param = grid.best_params_
-	print("Best params ", best_param)
-	print(grid.best_score_)
+best_param = grid.best_params_
+print("Best params ", best_param)
+print(grid.best_score_)
